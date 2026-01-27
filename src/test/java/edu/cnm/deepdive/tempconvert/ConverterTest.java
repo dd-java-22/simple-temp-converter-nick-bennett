7b72f00beae9f6601aa962d16ace95d7247ac065
Nick Bennett <nick@nickbenn.com>
2026-01-27 09:27:32 -0700

    Preparing for parameterized tests.

--- src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
+++ src/test/java/edu/cnm/deepdive/tempconvert/ConverterTest.java
@@ -3,13 +3,14 @@ package edu.cnm.deepdive.tempconvert;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 class ConverterTest {
 
   static final double TOLERANCE_SCALE = 0.0001;
 
-  @Test
-  void convertC2F() {
+  @ParameterizedTest
+  void convertC2F(double celsius, double expected) {
     assertEquals(
         -40,
         new Converter().convertC2F(-40),
