8c22cb6dcf1cdf638ddfd4b3824bf2c98743f49b
Nick Bennett <nick@nickbenn.com>
2026-01-27 15:40:53 -0700

    Preparing to handle user input.

---
+++ src/main/java/edu/cnm/deepdive/tempconvert/controller/InputHandler.java
@@ -0,0 +1,10 @@
+package edu.cnm.deepdive.tempconvert.controller;
+
+/**
+ * Handles user input from stdIn, using a Converter instance to perform conversions, with InputMode
+ * to select conversions, and writing output to stdOut.
+ */
+public class InputHandler {
+
+
+}
